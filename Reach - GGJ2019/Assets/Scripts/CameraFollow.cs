072b834439ec3dd82f606c8d2eaf2de64c44df0f
Graunder <graunder@gmail.com>
2019-01-25 22:46:24 +0200

    some logic + basic controlls

---
+++ Reach - GGJ2019/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothSpeed = 10f;
+    public Vector3 offset;
+
+    private void FixedUpdate() {
+
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothPosition;
+        //transform.LookAt(target);
+
+    }
+
+}
--- Reach - GGJ2019/Assets/Scripts/PlayerController.cs
+++ Reach - GGJ2019/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    
-    void Update()
-    {
-        
+
+    [Range(1, 10)]
+    public float jumpVelocity;
+
+    [Range(1, 10)]
+    public float runSpeed;
+
+    public float distToGround;
+    public bool grounded;
+
+    public bool CheckIfGrounded() {
+
+        grounded = Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.5f);
+        return grounded;
+
+    }
+
+    void FixedUpdate(){
+
+        if (Input.GetButtonDown ("Jump") && CheckIfGrounded()) {
+            GetComponent<Rigidbody>().velocity = Vector3.up * jumpVelocity;
+        }
+
+        this.transform.Translate(Input.GetAxis("Horizontal") * runSpeed * Time.deltaTime, 0, 0);
+
     }
 }
