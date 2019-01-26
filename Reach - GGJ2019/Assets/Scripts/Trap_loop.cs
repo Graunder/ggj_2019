c013f32e757086b390d7434bdbe499f5505988dd
rudolfswebdev <rudolfswebdev@gmail.com>
2019-01-26 18:50:44 +0200

    s

--- Reach - GGJ2019/Assets/Scripts/Trap_loop.cs
+++ Reach - GGJ2019/Assets/Scripts/Trap_loop.cs
@@ -8,17 +8,26 @@ public class Trap_loop : MonoBehaviour
    
     public GameObject lazer;
     private bool trapEnabled = true;
+    public GameObject particles;
 
     void Start()
     {
+        particles.SetActive(false);
         InvokeRepeating("ToogleTrap", 0, Random.Range(0.5f, 1.5f));
     }
 
     private void ToogleTrap() {
+        StartCoroutine(Fire());
+       
 
+    }
+    public IEnumerator Fire()
+    {
+        particles.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
         trapEnabled = !trapEnabled;
         lazer.SetActive(trapEnabled);
-
+        particles.SetActive(trapEnabled);
     }
 
 }
