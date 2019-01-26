59d3f1524600975b2525a95fc813796a23204ff7
Graunder <graunder@gmail.com>
2019-01-26 14:28:22 +0200

    went physics

---
+++ Reach - GGJ2019/Assets/Scripts/went.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class went : MonoBehaviour
+{
+    [Range(1, 50)]
+    public int forceX;
+    [Range(1, 50)]
+    public int forceY;
+    [Range(1, 50)]
+    public int forceZ;
+
+    private void OnTriggerStay(Collider other) {
+        if (other.CompareTag("Player")) {
+
+            other.attachedRigidbody.AddForce(forceX, forceY, forceZ);
+
+        }
+    }
+}
