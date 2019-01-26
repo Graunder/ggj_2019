adfa3fea4abd0010cda5d000cf9ba54c093e0d35
Graunder <graunder@gmail.com>
2019-01-26 10:23:16 +0200

    cinematci trap

---
+++ Reach - GGJ2019/Assets/Scripts/Trap_FallOnExit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap_FallOnExit : MonoBehaviour
+{
+
+    public GameObject trap;
+    public GameObject trigger;
+
+    private void OnTriggerExit(Collider trigger) {
+        if (trigger.CompareTag("Player")) {
+
+            stuffz();
+
+        }
+    }
+
+    void stuffz() {
+        Rigidbody trapRigidbody = trap.GetComponent<Rigidbody>();
+        trapRigidbody.isKinematic = false;
+    }
+
+}
