bfce1bd9a23f4011dbf897448050a6fd91dd76f8
Graunder <graunder@gmail.com>
2019-01-27 09:55:43 +0200

    Done?!

---
+++ Reach - GGJ2019/Assets/Scripts/exitGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class exitGame : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(quit());
+        }
+    }
+
+    IEnumerator quit() {
+
+        yield return new WaitForSeconds(2);
+        Application.Quit();
+
+    }
+}
